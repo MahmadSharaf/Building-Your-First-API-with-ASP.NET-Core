a950eafd0690f0b6a47314700215a7334d475106
Mohamed Sharaf <mahmad.sharaf@gmail.com>
2018-10-09 13:26:10 +0200

    Create CitiesController Class It contains all the actions: *To get a single city. or create ,or update one. *How these are consumed (Consistent e.i: consume all resources)

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc; //Controller
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    public class CitiesController : Controller
+    {
+        [HttpGet("api/cities")] //The routing template. This can connects a request with that url with this block
+        public JsonResult GetCities()//This returns a JSONified version of whaterever is passed into the constructor
+        {
+            List<object> l = new List<object>
+            // This creates a list of objects object is a reference data type. It is an alias for System.Object class
+              // The object types can be assigned values of any other types, value types, reference types, 
+              //  predefined or user-defined types. However, before assigning values, it needs type conversion.
+            {
+                new { id=1, Name="New York City" }, // List can be initialized while defining in this way instead of     
+                new { id=2, Name="Antwerp"}         // using list.Add(...)    
+            };
+
+            JsonResult result = new JsonResult(l);
+            return result;
+
+            /* // The above block can be summarized as follows
+             *
+            return new JsonResult(new List<object>
+                {
+                    new { id=1, Name="New York City" },
+                    new { id=2, Name="Antwerp"}
+                });*/   
+        }
+    }
+}
--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -8,27 +8,42 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CityInfo.API
-{
+{   //The startup class is the entry point of the application
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        /*This is used to add services to the container to configure those services.*/
+        /* A service is the component that intended for common consumptiin in an app
+         * There are frame work services, like identity, MVC, EF core services 
+         * There are application services
+         * There are built-in services, like the applicationBuilder and logger.*/
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /* It uses services that are registered and configured in that method. It's used to specify how an ASP.NET
+           Core application will respond to individual HTTP requests */
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-            }
+                app.UseDeveloperExceptionPage(); //This catches exceptions then handles the requests over to MVC, in such that,
+            }                                    // it returns the correct response when an exception happens in the MVC-related code
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            // After exceptions are handled
+            app.UseMvc(); // This requests pipelines
+
+            //app.Run(async (context) =>
+            //{
+            //    throw new Exception("EXample exception");
+                
+            //    //await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
