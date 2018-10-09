82337d1ee7fec161162a277f93b1dfe9e32f7869
Mohamed Sharaf <mahmad.sharaf@gmail.com>
2018-10-09 19:05:23 +0200

    Add Serialization The property names are returned as named in their classes

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 
 namespace CityInfo.API
 {   //The startup class is the entry point of the application
@@ -21,7 +22,15 @@ namespace CityInfo.API
          * There are built-in services, like the applicationBuilder and logger.*/
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .AddJsonOptions(o => {
+                     if (o.SerializerSettings.ContractResolver != null)
+                     {
+                         var castedResolver = o.SerializerSettings.ContractResolver
+                             as DefaultContractResolver;
+                         castedResolver.NamingStrategy = null;
+                     }
+                 });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
